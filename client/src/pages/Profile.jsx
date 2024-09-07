6c6cc3abebf59d4f9819f9a4031eb2f40729252d
Vishali Sharma <vishalisharma964@gmail.com>
2024-09-07 11:57:49 +0530

    create listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -136,6 +136,25 @@ const Profile = () => {
       
     }
   }
+  const handleListingDelete=async(listingId)=>{
+    try {
+      const res=await fetch(`/api/listing/delete/${listingId}`,{
+        method:'DELETE',
+
+      });
+      const data=await res.json();
+      if(data.success===false){
+        console.log(data.message);
+        return;
+
+      }
+      setUserListings((prev)=>prev.filter((listing)=>listing._id !==listingId)
+      )
+      
+    } catch (error) {
+      
+    }
+  }
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -217,7 +236,7 @@ const Profile = () => {
         <Link className="flex-1 text-slate-700 font-semibold  hover:underline truncate" to={`/listing/${listing._id}`}>
         <p >{listing.name}</p></Link>
         <div className="flex flex-col items-center">
-          <button className="text-red-700 uppercase">Delete</button>
+          <button onClick={()=>handleListingDelete(listing._id)} className="text-red-700 uppercase">Delete</button>
           <button className="text-green-700 uppercase">Edit</button>
 
        </div>
