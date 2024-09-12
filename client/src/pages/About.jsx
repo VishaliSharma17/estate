6d6ce8ee93385f68f645003c812a1d1284691757
Vishali Sharma <vishalisharma964@gmail.com>
2024-09-12 22:24:25 +0530

    about

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,14 +4,15 @@ const About = () => {
   return (
     <div className='py-20 px-4 max-w-6xl mx-auto'>
       <h1 className='text-3xl font-bold mb-4 text-slate-800 '>About Raj Estates</h1>
-     <p  className='mb-4 text-slate-700'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis sint neque odit aliquid. Adipisci laborum neque hic deleniti, minima porro quos fuga soluta placeat, sequi fugiat! Commodi consequatur perspiciatis debitis impedit eligendi, ratione quisquam vero atque nemo autem soluta omnis.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis sint neque odit aliquid. Adipisci laborum neque hic deleniti, minima porro quos fuga soluta placeat, sequi fugiat! Commodi consequatur perspiciatis debitis impedit eligendi, ratione quisquam vero atque nemo autem soluta omnis.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis sint neque odit aliquid. Adipisci laborum neque hic deleniti, minima porro quos fuga soluta placeat, sequi fugiat! Commodi consequatur perspiciatis debitis impedit eligendi, ratione quisquam vero atque nemo autem soluta omnis.
+     <p className='mb-4 text-slate-700 text-justify'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis sint neque odit aliquid. Adipisci laborum neque hic deleniti, minima porro quos fuga soluta placeat, sequi fugiat! Commodi consequatur perspiciatis debitis impedit eligendi, ratione quisquam vero atque nemo autem soluta omnis.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis sint neque odit aliquid. Adipisci laborum neque hic deleniti, minima porro quos fuga soluta placeat, sequi fugiat! Commodi consequatur perspiciatis debitis impedit eligendi, ratione quisquam vero atque nemo autem soluta omnis.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis sint neque odit aliquid. Adipisci laborum neque hic deleniti, minima porro quos fuga soluta placeat, sequi fugiat! Commodi consequatur perspiciatis debitis impedit eligendi, ratione quisquam vero atque nemo autem soluta omnis.
      </p>
-     <P className='mb-4 text-slate-700' >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni, voluptas laboriosam? Minima quae doloremque ipsa eius quis? Temporibus non iure perferendis provident, illum, voluptas, quaerat ducimus beatae reprehenderit cumque neque!
+     <p className='mb-4 text-slate-700 text-justify' >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni, voluptas laboriosam? Minima quae doloremque ipsa eius quis? Temporibus non iure perferendis provident, illum, voluptas, quaerat ducimus beatae reprehenderit cumque neque!
      Nihil, pariatur dolore ad vitae optio doloribus dolorem ipsa obcaecati nesciunt nemo aliquid quis, libero quasi velit laudantium sequi quibusdam, voluptatibus iure maiores tenetur natus explicabo voluptas tempora suscipit. Sed?
      Odit adipisci maiores dolores, laboriosam soluta sunt nemo ducimus? Minima dolorum repudiandae iste, deleniti molestias debitis doloremque quam quis illum officiis, a aut ad voluptatem quisquam est. Aperiam, fugit ad.
      Harum fuga quae vitae, illum voluptates architecto atque perspiciatis, corporis cum, libero et! Error, deleniti a itaque quaerat porro earum suscipit ipsum, corporis esse repellat, incidunt eum consequatur dignissimos sapiente.
-     At nemo et debitis. Necessitatibus cumque ipsum totam et, aut omnis enim provident odio sit sint, quidem earum autem atque fuga id explicabo, voluptatum quae voluptas perspiciatis neque molestiae itaque!</P>
+     At nemo et debitis. Necessitatibus cumque ipsum totam et, aut omnis enim provident odio sit sint, quidem earum autem atque fuga id explicabo, voluptatum quae voluptas perspiciatis neque molestiae itaque!</p>
     </div>
+   
   )
 }
 
